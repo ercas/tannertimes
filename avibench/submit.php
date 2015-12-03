80ae000c37eb2cb84608a4480458b7ed3f112f64
ercas <ercas@users.noreply.github.com>
2015-12-03 17:12:32 -0500

    more benchmarks, added a form and prototype php handler

--- avibench/submit.php
+++ avibench/submit.php
@@ -5,20 +5,23 @@
         <link rel=stylesheet type=text/css href=style.css>
     </head>
     <body>
-        <img src=header.png style="display: block; margin: 0 auto;">
+        <a target=_blank href=http://thetannertimes.net><img src=header.png style="display: block; margin: 0 auto;"></a>
         <br>Thank you for participating!
         <br>
         <br>
-        <div id=output>
-            <?php
-                $data = $_SERVER["REQUEST_TIME"] . "," . $_SERVER["REMOTE_ADDR"] . "," . $_SERVER["HTTP_USER_AGENT"] . "\n";
-                file_put_contents("responses.txt", $data, FILE_APPEND | LOCK_EX) or die("error");
+        Received: 
+        <pre><code><?php echo $_POST["results"]; ?></code></pre>
+        <br>
+        <?php
+            echo "your user agent: " . $_SERVER["HTTP_USER_AGENT"];
+            echo "<br>your ip address: " . $_SERVER["REMOTE_ADDR"];
+            echo "<br><br>submitted at " . gmdate("Y-m-d\TH:i:s\Z",$_SERVER["REQUEST_TIME"]);
+            echo "<br><br>server info: " . $_SERVER["SERVER_SOFTWARE"] . " port " . $_SERVER["SERVER_PORT"]; 
+        ?>
 
-                echo "your user agent: " . $_SERVER["HTTP_USER_AGENT"];
-                echo "<br>your ip address: " . $_SERVER["REMOTE_ADDR"];
-                echo "<br><br>submitted at " . gmdate("Y-m-d\TH:i:s\Z",$_SERVER["REQUEST_TIME"]);
-                echo "<br><br>server info: " . $_SERVER["SERVER_SOFTWARE"] . " port " . $_SERVER["SERVER_PORT"]; 
-            ?>
+        <div id=footer>
+            <br>
+            <br>The source code for this project is available on <a target=_blank href=http://github.com/ercas/tannertimes/tree/master/avibench>GitHub</a>.
         </div>
     </body>
 </html>
