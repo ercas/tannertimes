be1d6d747ba1a197375be092d88bdc269c61abad
ercas <ercas@users.noreply.github.com>
2015-12-28 21:53:35 -0500

    added rotation

--- superimposer/main.lua
+++ superimposer/main.lua
@@ -1,21 +1,29 @@
 -- usage: love . /path/to/images/*.jpg
 
+-- config
+imageExtensions = {
+    "jpg",
+    "png",
+}
+rotationSensitivity = 0.01
+scaleSensitivity = 0.01
+
+-- image properties
 images = {}
 baseImage = nil
 overlayImage = nil
 position = {x = 0, y = 0}
-
-mouseDown = false
-mouseOriginalPosition = {x = 0, y = 0}
-mouseDragged = {x = 0, y = 0}
+rotation =  0
 scale = 1
 
-imageExtensions = {
-    "jpg",
-    "png",
-    "gif",
-    "tiff"
-}
+-- used for controls
+dragging = false
+mouseOriginalPosition = {x = 0, y = 0}
+mouseDragged = {x = 0, y = 0}
+
+rotating = false
+mouseRotationOrigin = 0
+mouseRotated = 0
 
 --== functions ==============================================================---
 -- check if a given file has an image extension (as specified by the
@@ -34,43 +42,61 @@ function imageFromPath(path)
     local file = io.open(path)
     local imageDataRaw = file:read("*all")
     file:close()
-    local imageData = love.filesystem.newFileData(imageDataRaw,path:match("[^/]+$"))
+    local imageData = love.filesystem.newFileData(imageDataRaw, path:match("[^/]+$"))
     local imageObject = love.graphics.newImage(imageData)
     return imageObject
 end
 
 --== input ==================================================================---
-function love.mousepressed(x,y)
-    print("mouse pressed")
-    mouseDown = true
-    mouseOriginalPosition = {x = x, y = y}
+function love.mousepressed(x, y, button)
+    print("mouse pressed", button)
+    if button == 1 then
+        dragging = true
+        mouseOriginalPosition = {x = x, y = y}
+    elseif button == 2 then
+        rotating = true
+        mouseRotationOrigin = x
+    end
 end
 
-function love.mousemoved(x,y)
-    if mouseDown then
-        print("mouse dragging",
-            mouseOriginalPosition.x - x, mouseOriginalPosition.y - y)
+function love.mousemoved(x, y, button)
+    if dragging then
         mouseDragged = {
             x = -(mouseOriginalPosition.x - x),
             y = -(mouseOriginalPosition.y - y)
         }
+        print("mouse dragging", mouseDragged.x, mouseDragged.y, button)
+    end
+    -- not elseif because a user could be dragging and rotating at the same time
+    if rotating then
+        mouseRotated = (mouseRotationOrigin - x) * rotationSensitivity
+        print("mouse rotating", mouseRotated)
     end
 end
 
-function love.mousereleased(x,y)
-    print("mouse released")
-    mouseDown = false
-    position = {
-        x = position.x + mouseDragged.x,
-        y = position.y + mouseDragged.y
-    }
-    mouseDragged = {x = 0, y = 0}
-    print("mouse dragged", mouseDragged.x, mouseDragged.y)
+function love.mousereleased(x, y, button)
+    print("mouse released", button)
+    if button == 1 then
+        print("total drag", mouseDragged.x, mouseDragged.y)
+        dragging = false
+        position = {
+            x = position.x + mouseDragged.x,
+            y = position.y + mouseDragged.y
+        }
+        print("new position", position.x, position.y)
+        mouseDragged = {x = 0, y = 0}
+    elseif button == 2 then
+        print("total rotation", mouseRotated)
+        rotating = false
+        rotation = rotation + mouseRotated
+        print("new rotation", rotation)
+        mouseRotated = 0
+    end
 end
 
-function love.wheelmoved(x,y)
+function love.wheelmoved(x, y)
     print("scroll", x, y)
-    scale = scale + (y * 0.01)
+    scale = scale + (y * scaleSensitivity)
 end
 
 --===========================================================================---
@@ -97,18 +123,19 @@ function love.load(args)
         print(index, value)
     end
 
-    baseImage = imageFromPath(table.remove(images,1))
+    baseImage = imageFromPath(table.remove(images, 1))
 end
 
 function love.draw()
     if not overlayImage then
-        overlayImage = imageFromPath(table.remove(images,1))
+        overlayImage = imageFromPath(table.remove(images, 1))
     end
     love.graphics.draw(baseImage)
     love.graphics.draw(
         overlayImage,
         position.x + mouseDragged.x,
         position.y + mouseDragged.y,
-        0,scale,scale
+        rotation + mouseRotated,
+        scale, scale
     )
 end
